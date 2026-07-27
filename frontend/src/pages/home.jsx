038b0e4892b2130a4abcd297a2744f6dd02013f7
Tammyboy23 <fagboyeguntamilore43@gmail.com>
2026-07-27 22:36:20 +0100

    version 1 complete

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,12 +1,38 @@
+import { useState } from 'react'
 import img1 from '../assets/caucasian-family-is-enjoying-summer-vacation.jpg'
 import img2 from '../assets/happy-woman-playing-arcade-game.jpg'
 import img3 from '../assets/imgi_146_family-beach-with-sunset-background_1072992-1546.png'
+import { LuHandshake, LuLaugh, LuSend } from 'react-icons/lu'
+import { DotLoader, RingLoader, ScaleLoader } from 'react-spinners'
+import { toast } from 'react-toastify'
 function Home(){
+    const [email, setemail] = useState("")
+    const [load, setload] = useState(false)
+    const submit = () => {
+        setload(true)
+        fetch("https://fun-spot.onrender.com/list",{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email})
+        })
+        .then(res => res.json())
+        .then((data) => {
+            toast.success(data.message);
+            setload(false)
+        })
+        .catch((err) => {
+            console.log(err)
+            toast.error(err.message || err)
+            setload(false)
+        })
+    }
     return(
         <>
         <div className="land">
             <nav>
-                <h1>Fun <span>Spot</span></h1>
+                <h1>Fun <span>Spot <LuLaugh /></span></h1>
                 <button>Join List</button>
             </nav>
             <div className="hero">
@@ -25,31 +51,69 @@ function Home(){
             </div>
             <div className="how">
                 <h2>How It works ?</h2>
-                <p>Detailed Explanation on how fun spot can help you</p>
+                <h5>Detailed Explanation on how fun spot can help you</h5>
                 <div className="how-container">
                 <div className="how-card">
-                    <span>1</span>
+                    <span>STEP 1</span>
                     <div className="card-detail">
                         <h3>Insert Information</h3>
                         <p>You have to input the location u want to have your outing the theme the number of people and budget aand let fun spot do the rest</p>
                     </div>
                 </div>
                 <div className="how-card">
-                    <span>1</span>
+                    <span>STEP 2</span>
                     <div className="card-detail">
-                        <h3>Insert Information</h3>
-                        <p>You have to input the location u want to have your outing the theme the number of people and budget aand let fun spot do the rest</p>
+                        <h3>Processing Request</h3>
+                        <p>Behind the scenes Ai processes all the information you have given to it and now it searches of a place wihtin your desired location to have fun with your poeple all within ur said budget</p>
                     </div>
                 </div>
                 <div className="how-card">
-                    <span>1</span>
+                    <span>STEP 3</span>
                     <div className="card-detail">
-                        <h3>Insert Information</h3>
-                        <p>You have to input the location u want to have your outing the theme the number of people and budget aand let fun spot do the rest</p>
+                        <h3>Have Fun</h3>
+                        <p>If it works the way it is supposed to u should have multiple locations that fit within ur description and a plan on  how to spend your budget there plus a google maps link to the location</p>
                     </div>
                 </div>
                 </div>
             </div>
+            <div className="join">
+                <div className="join-card">
+                    <h1>Join Now <LuHandshake /></h1>
+                    <h3>Be Amongst the first to make use of this product when complete</h3>
+                    <div className="join-form">
+                        <input type="email" placeholder='example@gmail.com' value={email} onChange={(e) => setemail(e.target.value)}/>
+                        <button onClick={submit} disabled={load}>
+                            {load ? (
+                                <><span>Joining</span><ScaleLoader height={16} width={4} color='white' /></>
+                            ) : (
+                                <><span>Join Waitlist</span><LuSend /></>
+                            )}
+                        </button>
+                    </div>
+                </div>
+            </div>
+           <div className="faq">
+            <h1>Frequently Asked Questions</h1>
+            <details>
+                <summary>How doest it plan my outings?</summary>
+                <p>It uses AI to get location within your budget and one that matches ur theme.</p>
+            </details>
+            <details>
+                <summary>Do I have to pay for fun spot ?</summary>
+                <p>No fun spot is completely free for now until premium features are added.</p>
+            </details>
+            <details>
+                <summary>When is it going to be released ?</summary>
+                <p>The time of release for fun spot is unknown but should be sometime this month.</p>
+            </details>
+            <details>
+                <summary>Who created Fun Spot ?</summary>
+                <p>Fun Spot was created by fagboyegun tamilore a software engineer </p>
+            </details>
+           </div>
+            <footer>
+                <p>Copyright &copy; {new Date().getFullYear()} built by Fagboyegun Oluwatamilore</p>
+            </footer>
         </div>
         </>
     )
